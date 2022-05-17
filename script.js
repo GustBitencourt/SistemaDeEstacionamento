07b81f6032b61799f84443a01775e82a5db85645
GustBitencourt <gubiten@gmail.com>
2022-05-17 10:43:26 -0300

    ajuste do cadastro para receber informacoes, criacao de interface veiculo, funcao de adicionar

--- script.js
+++ script.js
@@ -4,13 +4,36 @@
     /* atalho pra usar query selector */
     /* HTMLInputElement entende que todas as informações/eventos serão de input */
     const $ = (query) => document.querySelector(query);
+    function patioHandle() {
+        function read() { }
+        function add(veiculo) {
+            var _a;
+            const row = document.createElement("tr");
+            //criando tabela
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+                <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+            //inserindo elemento no HTML
+            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function remove() { }
+        function save() { }
+        function render() { }
+        return { read, add, remove, save, render };
+    }
     (_a = $('#cadastrar')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
         const placa = (_b = $('#placa')) === null || _b === void 0 ? void 0 : _b.value;
-        if (nome || placa) {
-            alert('Nome e laca do veiculo obrigatórios');
+        if (!nome || !placa) {
+            alert('Nome e placa do veiculo obrigatórios');
             return;
         }
+        patioHandle().add({ nome, placa, entrada: new Date() });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,16 +1,53 @@
+interface Veiculo {
+    nome?: String;
+    placa?: String;
+    entrada?: Date;
+}
+
 (function () {
     /* atalho pra usar query selector */
     /* HTMLInputElement entende que todas as informações/eventos serão de input */
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
 
+    function patioHandle() {
+        function read(){}
+
+        function add(veiculo: Veiculo) {
+            const row = document.createElement("tr");
+
+            //criando tabela
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+                <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+
+            //inserindo elemento no HTML
+            $('#patio')?.appendChild(row);
+        }
+
+        function remove(){}
+
+        function save(){}
+
+        function render(){}
+
+        return { read, add, remove, save, render };
+    }
+
     $('#cadastrar')?.addEventListener("click", () => {
         const nome = $('#nome')?.value;
         const placa = $('#placa')?.value;
 
-        if (nome || placa) {
-            alert('Nome e laca do veiculo obrigatórios');
+        if (!nome || !placa) {
+            alert('Nome e placa do veiculo obrigatórios');
             return;
         }
+
+        patioHandle().add({ nome, placa, entrada: new Date() });
     })
 
 })();
